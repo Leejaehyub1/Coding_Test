c79c4019527d5cae973d7c93b55d56080a3bed01
Lee JaeHyub <jae213@ajou.ac.kr>
2021-05-17 03:21:52 +0900

    May 17

--- List.py
+++ List.py
@@ -9,3 +9,25 @@ stack.append('b')
 stack.append('c')
 stack.pop(-1)   #pop(), pop(-1) 똑같음. -> c가 나옴
 stack.pop(0)    #첫번째 원소 pop (Queue 에서 사용) -> a가 나옴
+
+
+
+
+
+#append와 extend의 차이점.
+#extend는 iterable한 모든 항목을 넣는다.
+x = ['a', 'b', 'c']
+y = ['d', 'e']
+x.append(y)
+print(x) #['a', 'b', 'c', ['d', 'e']]
+
+x = ['a', 'b', 'c']
+y = ['d', 'e']
+x.extend(y)
+print(x) #['a', 'b', 'c', 'd', 'e']
+
+#만약 문자열이면?
+x = ['aaa', 'bbb', 'ccc']
+y = 'def'
+x.extend(y)
+print(x) #['aaa', 'bbb', 'ccc', 'd', 'e', 'f']
