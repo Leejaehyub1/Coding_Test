3008517b8ab119157e74cdaa0358e2a78bc8242c
Lee JaeHyub <jae213@ajou.ac.kr>
2021-05-23 17:51:29 +0900

    May 23

--- String.py
+++ String.py
@@ -35,11 +35,11 @@ s = 'this is {0:*^10} | done {1:^5} |'.format('center', 'c')
 
 
 # 자리수와 소수점 표현하기
-s1 = '정수 3자리 : {0:03d, {1:03d}'.format(12345,12)
+s1 = '정수 3자리 : {0:03d}, {1:03d}'.format(12345, 12)
 # print(s1)
 # 12345, 012
 
-s2 = '아래 2자리 : {0:0.2f, {1:0.5f}'.format(123.1234567, 3.14)
+s2 = '아래 2자리 : {0:0.2f}, {1:0.5f}'.format(123.1234567, 3.14)
 #123.12, 3.14000
 
 # %s 사용 가능
@@ -50,4 +50,37 @@ s = 'Example number is %d, Example String is %s'%(a,b)
 
 
 
+# 문자열을 대문자/소문자로 변경
+s1 = 'LeeJaehyub'
+s2 = s1.upper()
+s3 = s1.lower()
+print(s2)   #LEEJAEHYUB
+print(s3)   #leejaehyub
 
+
+# 문자열을 대문자/소문자 확인
+# 모든 문자가 대문자인지? 혹은 소문자인지?
+s1.isupper()
+s1.islower()
+
+
+# find, rfind, index
+# find 함수: "문자열"만 사용 가능. 찾는 문자가 없는 경우 return -1
+# rfind 함수: 역순으로 find
+# index 함수: 문자열, 리스트, 튜플에서 사용 가능. 찾는 문자가 없으면 Error
+
+
+
+
+
+#십진법, 이진법 변경
+
+# 문자열 이진법을 십진법 int로 변경하기
+a = '1011'
+ans = int(a,2)
+print(ans)
+
+# 십진법을 이진법으로 변경
+a = 5
+temp = bin(a)    #0b101
+ans = temp[2:]   #101
