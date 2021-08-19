b18e7df44e924cec3621d608291f18d08a5b3df9
Jaehyub1 <jae213@ajou.ac.kr>
2021-08-19 16:12:24 +0900

    2021-08-19

--- 백준/[9184번]_신나는 함수 실행.py
+++ 백준/[9184번]_신나는 함수 실행.py
@@ -1,6 +1,5 @@
 ans = [[[0]*21 for _ in range(21)] for _ in range(21)]
 def w(a,b,c):
-
     if (a <= 0 or b <= 0 or c <= 0):
         return 1
 
@@ -18,14 +17,13 @@ def w(a,b,c):
         ans[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
         return ans[a][b][c]
 
-
-
-
-
 while(True):
     a, b, c = map(int, input().split(" "))
     if(a==-1 and b==-1 and c==-1):
         break
     print('w({0}, {1}, {2}) = {3}'.format(a,b,c,w(a,b,c)))
+
+
+
 
 
