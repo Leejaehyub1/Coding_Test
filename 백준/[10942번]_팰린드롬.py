efe7497517a03fa12d8e6ae0087ed634e4a983e1
LeeJaehyub <jae213@ajou.ac.kr>
2021-08-23 17:40:01 +0900

    2021-08-23

---
+++ 백준/[10942번]_팰린드롬.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+numbers = list(map(int, input().split()))
+m = int(input())
+
+dp = [[0]*n for _ in range(n)]
+
+for num_len in range(n):
+    for start in range(n-num_len):
+        end = start + num_len
+
+        if start == end:
+            dp[start][end] = 1
+        elif numbers[start] == numbers[end]:
+            if (start+1 == end) or dp[start+1][end-1] == 1:
+                dp[start][end] = 1
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(dp[s-1][e-1])
