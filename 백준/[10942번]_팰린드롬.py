49c9e7ed8f1617111ca94cd339af05163d61ffec
LeeJaehyub <jae213@ajou.ac.kr>
2021-08-29 19:21:28 +0900

    2021-08-29

--- 백준/[10942번]_팰린드롬.py
+++ 백준/[10942번]_팰린드롬.py
@@ -10,10 +10,10 @@ for num_len in range(n):
     for start in range(n-num_len):
         end = start + num_len
 
-        if start == end:
+        if start == end: #길이가 1이다.
             dp[start][end] = 1
-        elif numbers[start] == numbers[end]:
-            if (start+1 == end) or dp[start+1][end-1] == 1:
+        elif numbers[start] == numbers[end]: #처음과 끝이 똑같다.
+            if (start+1 == end) or dp[start+1][end-1] == 1: #길이가 2이거나, 앞뒤 짤라낸 결과가 팰린드롬인 경우.,
                 dp[start][end] = 1
 
 for _ in range(m):
