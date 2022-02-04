01d888424aede60c1c1d3574bb97f7645705292c
이재협 <jae213@ajou.ac.kr>
2022-02-04 16:52:13 +0900

    2022-02-04

--- Programmers/[Programmers]_Level2_기능 개발.py
+++ Programmers/[Programmers]_Level2_기능 개발.py
@@ -1,19 +1,48 @@
-def solution(progresses, speeds):
-    answer = []
-    for i in range(len(progresses)):
-        progresses[i] = 100 - progresses[i]
-        if (progresses[i] % speeds[i]):
-            progresses[i] = (progresses[i] // speeds[i]) + 1
-        else:
-            progresses[i] = (progresses[i] // speeds[i])
+# def solution(progresses, speeds):
+#     answer = []
+#     for i in range(len(progresses)):
+#         progresses[i] = 100 - progresses[i]
+#         if (progresses[i] % speeds[i]):
+#             progresses[i] = (progresses[i] // speeds[i]) + 1
+#         else:
+#             progresses[i] = (progresses[i] // speeds[i])
+#
+#     while (len(progresses) != 0):
+#         num = 1
+#         temp = progresses.pop(0)
+#         while (len(progresses) != 0 and progresses[0] <= temp):
+#             num += 1
+#             progresses.pop(0)
+#
+#         answer.append(num)
+#
+#     return answer
 
-    while (len(progresses) != 0):
-        num = 1
-        temp = progresses.pop(0)
-        while (len(progresses) != 0 and progresses[0] <= temp):
-            num += 1
-            progresses.pop(0)
+a = list(map(int, input().split()))
+a.sort()
 
-        answer.append(num)
+min = 10000000
+def averge(a):
+    sum = 0
+    for i in a:
+        sum+= i
+    return sum//len(a)
 
-    return answer
+min = 1000000
+for i in range(1, len(a)):
+    b = a[:i]
+    c = a[i:]
+    avg_b = averge(b)
+    avg_c = averge(c)
+    sum = 0
+    for k in b:
+        sum += abs(avg_b-k)
+    for k in c:
+        sum += abs(avg_c-k)
+    if sum < min:
+        min = sum
+
+
+print(min)
+
+
