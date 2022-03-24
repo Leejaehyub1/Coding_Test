755f9261c460fb2cd56387dfef2092ca14d9195d
이재협 <leejh8136@naver.com>
2022-03-25 00:10:31 +0900

    SWEA

--- SWEA/SWEA_7465.java
+++ SWEA/SWEA_7465.java
@@ -28,7 +28,7 @@ public class SWEA_7465 {
 	}
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = null;
 		int T = Integer.parseInt(br.readLine());
 		for(int test_case=1; test_case<=T;test_case++) {
 			st = new StringTokenizer(br.readLine());
