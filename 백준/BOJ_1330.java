21022da9d35dc47af499c24352fe2d3af8aaa6b9
이재협 <leejh8136@naver.com>
2022-03-24 23:53:12 +0900

    BOJ_1330

--- 백준/BOJ_1330.java
+++ 백준/BOJ_1330.java
@@ -1,9 +1,27 @@
 package algorithm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_1330 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		if(A < B){
+			System.out.println("<");
+		}
+		else if(A==B) {
+			System.out.println("==");
+		}
+		
+		
+		else {
+			System.out.println(">");
+		}
 
 	}
 
