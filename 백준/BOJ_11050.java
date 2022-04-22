8e842ed90826b89109d70cc4861cfe0bfb3003a4
이재협 <leejh8136@naver.com>
2022-04-22 20:27:18 +0900

    2022-04-22

---
+++ 백준/BOJ_11050.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11050 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+
+	}
+
+}
